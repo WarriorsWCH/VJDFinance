c1c0339a0b63a2b6f7ebc514cf772b0335e7d208
groot <328592171@qq.com>
2018-02-26 11:34:35 +0800

    feat(module): money

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "@/components/home/index.vue"
-// import Money from "@/components/money/index.vue"
+import Money from "@/components/money/index.vue"
 // import Ious from "@/components/ious/index.vue"
 // import Raise from "@/components/raise/index.vue"
 // import Download from "@/components/special/download.vue"
@@ -17,11 +17,12 @@ export default new Router({
             name: "home",
             component: Home,
         }, 
+        {
+            path: "/money",
+            name: "money",
+            component: Money,
+        }, 
         // {
-        //     path: "/money",
-        //     name: "money",
-        //     component: Money,
-        // }, {
         //     path: "/ious",
         //     name: "ious",
         //     component: Ious,
