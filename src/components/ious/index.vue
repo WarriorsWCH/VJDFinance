72b6ef4d8b23902617d52b66dec2e9d970f165d6
groot <328592171@qq.com>
2018-02-26 14:12:25 +0800

    feat(module): ious

---
+++ src/components/ious/index.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+    <div>
+        <Heador/>
+        <Slider/>
+        <Welfare/>
+        <Service/>
+        <Footor/>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from "../common/header.vue"
+import Footor from "../common/footer.vue"
+import Navbar from "../common/navbar.vue"
+import Slider from "./islider.vue"
+import Welfare from "./welfare.vue"
+import Service from "./service.vue"
+export default {
+    components: {
+        Heador,
+        Slider,
+        Welfare,
+        Service,
+        Footor,
+        Navbar,
+    },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "@/components/home/index.vue"
 import Money from "@/components/money/index.vue"
-// import Ious from "@/components/ious/index.vue"
+import Ious from "@/components/ious/index.vue"
 // import Raise from "@/components/raise/index.vue"
 // import Download from "@/components/special/download.vue"
 import "../css/reset.scss"
@@ -22,11 +22,12 @@ export default new Router({
             name: "money",
             component: Money,
         }, 
+        {
+            path: "/ious",
+            name: "ious",
+            component: Ious,
+        }, 
         // {
-        //     path: "/ious",
-        //     name: "ious",
-        //     component: Ious,
-        // }, {
         //     path: "/raise",
         //     name: "raise",
         //     component: Raise,
