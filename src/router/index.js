86a659679810b4450bcaa2eb3e202a8cda0bb0c9
groot <328592171@qq.com>
2018-02-26 14:24:08 +0800

    feat(module): raise

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,7 @@ import Router from "vue-router"
 import Home from "@/components/home/index.vue"
 import Money from "@/components/money/index.vue"
 import Ious from "@/components/ious/index.vue"
-// import Raise from "@/components/raise/index.vue"
-// import Download from "@/components/special/download.vue"
+import Raise from "@/components/raise/index.vue"
 import "../css/reset.scss"
 
 Vue.use(Router)
@@ -27,15 +26,11 @@ export default new Router({
             name: "ious",
             component: Ious,
         }, 
-        // {
-        //     path: "/raise",
-        //     name: "raise",
-        //     component: Raise,
-        // }, {
-        //     path: "/download",
-        //     name: "download",
-        //     component: Download,
-        // },
+        {
+            path: "/raise",
+            name: "raise",
+            component: Raise,
+        },
     ],
 })
 
